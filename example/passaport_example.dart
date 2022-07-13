ea23a57c8d3977c17a91ca71b0522cf57bba5173
Jacob Moura <jacobaraujo7@gmail.com>
2022-07-13 09:03:37 -0300

    first commit

---
+++ example/passaport_example.dart
@@ -0,0 +1,6 @@
+import 'package:passaport/passaport.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/passaport.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library passaport;
+
+export 'src/passaport_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
